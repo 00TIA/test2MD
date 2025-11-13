74e882a6052e5f54f14b595dd085a0c5839fe259
MattiaGadda <mattia.gadda@mercurio.solutions>
2025-11-13 10:40:59 +0100

    create Project

---
+++ test2MD/test2MD/test2MDApp.swift
@@ -0,0 +1,17 @@
+//
+//  test2MDApp.swift
+//  test2MD
+//
+//  Created by mattia gadda on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct test2MDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test2MD/test2MDTests/test2MDTests.swift
@@ -0,0 +1,17 @@
+//
+//  test2MDTests.swift
+//  test2MDTests
+//
+//  Created by mattia gadda on 13/11/25.
+//
+
+import Testing
+@testable import test2MD
+
+struct test2MDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
